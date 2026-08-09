eda29e62f739612f78181e2fb7ea040098ae2081
freroxx <yikni2829@gmail.com>
2026-08-09 16:11:30 +0100

    UPDATED GSMARENA API

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const deals = require('./src/services/deals');
 const glossary = require('./src/services/glossary');
 const search = require('./src/services/search');
 const top = require('./src/services/top');
+const utils = require('./src/services/utils');
 
 module.exports = {
     catalog,
     deals,
     glossary,
-    search,
+    search: search.search,
+    discoverDevice: search.discoverDevice,
     top,
+    utils,
+    // Direct exports for common utilities
+    fetchHtml: utils.fetchHtml,
+    generateSmartStrategies: utils.generateSmartStrategies,
+    isTurnstile: utils.isTurnstile,
+    getRemainingTime: utils.getRemainingTime,
 };
